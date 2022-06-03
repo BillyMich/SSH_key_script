5141e951182f9cc82c83cb92b2e829220846f70b
BillyMich <michaleasvas@gmail.com>
2022-06-03 10:32:21 +0300

    Upadated

--- ssh.py
+++ ssh.py
@@ -3,5 +3,14 @@ import subprocess
 
 print('Type your email your git using!')
 sting_email=input()
-os.chdir("/home/")
+os.chdir("/")
 os.system("ssh-keygen -t ed25519 -C %s "% sting_email)
+os.system("eval $(ssh-agent -s)")
+#missing the touch and write in ~/.ssh/config 
+#fp = open(' ~/.ssh/config', 'w')
+#fp.write(' AddKeysToAgent yes')
+#fp.write(' IdentityFile ~/.ssh/id_ed25519')
+os.system("cat ~/.ssh/id_ed25519.pub")
+print('Coppy this and past it in ssh key generator on git')
+
+
